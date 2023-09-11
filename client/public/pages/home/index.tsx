af3097d9e37ff11ff56c334a470946cc85079d8e
Nanai Jua <me@n7i.dev>
2023-09-11 23:57:03 +0900

    feat(client): roughly implement of api gluing

--- client/public/pages/home/index.tsx
+++ client/public/pages/home/index.tsx
@@ -1,15 +1,23 @@
-import { useCallback, useReducer } from "preact/hooks";
-import { z } from "zod";
+import "preact/debug";
 
-type Post = z.TypeOf<typeof Post>;
-const Post = z.object({
-  content: z.string(),
-});
+import { useAPI, Post } from "../../api";
+import { useEffect, useCallback, useState } from "preact/hooks";
+
+const fmts: Intl.DateTimeFormatOptions = {
+  dateStyle: "medium",
+  timeStyle: "medium",
+};
 
 const ShowPost = ({ post }: { post: Post }) => {
   return (
     <div class="">
       <p class="">{post.content}</p>
+      <time
+        class="block mt-4 opacity-50 text-right"
+        dateTime={post.postedAt.toISOString()}
+      >
+        {post.postedAt.toLocaleString("ja-JP", fmts)}
+      </time>
     </div>
   );
 };
@@ -17,7 +25,7 @@ const ShowPost = ({ post }: { post: Post }) => {
 const Timeline = ({ posts }: { posts: Post[] }) => {
   return (
     <ul class="w-full divide-(y slate-300)">
-      {posts.map((post) => (
+      {posts.toReversed().map((post) => (
         <li class="px-2 py-4">
           <ShowPost post={post} />
         </li>
@@ -26,52 +34,41 @@ const Timeline = ({ posts }: { posts: Post[] }) => {
   );
 };
 
-const assert = (fn: () => boolean): void | never => {
-  if (!fn()) {
-    throw new Error("Assertion failed: " + fn.toString());
-  }
-};
+const Submit = ({ reload }: { reload: () => void }) => {
+  const [text, setText] = useState("");
 
-const submitReducer = (
-  state: Partial<Post> & { callback: (p: Post) => void },
-  action:
-    | { ty: "update"; v: Partial<Post & { callback: (p: Post) => void }> }
-    | { ty: "submit" },
-) => {
-  switch (action.ty) {
-    case "update":
-      return { ...state, ...action.v };
+  const update = useCallback(
+    (e: Event) => {
+      if (e.target instanceof HTMLTextAreaElement) {
+        setText(e.target.value);
+      } else {
+        throw new Error("Unexpected event target: " + e.target);
+      }
+    },
+    [setText],
+  );
 
-    case "submit":
-      state.callback(Post.parse(state));
-      return { callback: state.callback };
-  }
-};
+  const [post, loading, fire] = useAPI("/posts", "POST");
 
-const Submit = ({ callback }: { callback: (p: Post) => void }) => {
-  const [state, dispatch] = useReducer(submitReducer, { callback });
+  const submit = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === "Enter") {
+        setText((text) => {
+          fire({ content: text });
+          return "";
+        });
 
-  if (state.callback !== callback) dispatch({ ty: "update", v: { callback } });
-
-  // rome-ignore format:
-  const update = useCallback((e: Event) => {
-    if (e.target instanceof HTMLTextAreaElement) {
-      dispatch({ ty: "update", v: { content: e.target.value } });
-    } else {
-      throw new Error("Unexpected event target: " + e.target);
-    }
-  }, [dispatch]);
-
-  // rome-ignore format:
-  const submit = useCallback((e: KeyboardEvent) => {
-    if (e.ctrlKey && e.key === "Enter") dispatch({ ty: "submit" });
-  }, [dispatch]);
+        reload();
+      }
+    },
+    [fire, setText],
+  );
 
   return (
     <textarea
       class="p-2 w-full bg-slate-100 rounded-xl border-(2 slate-300) resize-none"
       rows={3}
-      value={state.content ?? ""}
+      value={text}
       onInput={update}
       onKeyDown={submit}
     />
@@ -79,14 +76,12 @@ const Submit = ({ callback }: { callback: (p: Post) => void }) => {
 };
 
 export default function Home() {
-  const [posts, append] = useReducer<Post[], Post>(
-    (posts, additional) => [...posts, additional],
-    [
-      { content: "こんにちは！" },
-      { content: "こんにちは！" },
-      { content: "こんにちは！" },
-    ],
-  );
+  const [posts, loading, fire] = useAPI("/posts", "GET");
+
+  const get = useCallback(() => fire(null), [fire]);
+
+  // initially load
+  useEffect(get, [get]);
 
   return (
     <main class="w-full min-h-screen flex flex-row">
@@ -96,11 +91,11 @@ export default function Home() {
 
       <div class="basis-7/12 flex flex-col">
         <div class="grow overflow-y-auto">
-          <Timeline posts={posts} />
+          <Timeline posts={posts ?? []} />
         </div>
 
         <div class="mb-4">
-          <Submit callback={append} />
+          <Submit reload={get} />
         </div>
       </div>
 
