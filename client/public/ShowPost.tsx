6fb730b460d548f379f9e1fa2e9137da64af2886
Nanai Jua <me@n7i.dev>
2023-09-27 08:54:32 +0900

    fix(client): revived animation on hover

--- client/public/ShowPost.tsx
+++ client/public/ShowPost.tsx
@@ -1,12 +1,28 @@
+import { injectGlobal } from "@twind/core";
+
 import { Post } from "./api";
 
 import { FormatDate } from "./FormatDate";
 import { Markdown } from "./Markdown";
 import { PostMenu } from "./PostMenu";
 
+injectGlobal`
+  .has-menu > .menu {
+    @apply opacity-0;
+  }
+
+  .has-menu:hover > .menu {
+    @apply opacity-100;
+  }
+
+  .menu {
+    @apply transition-opacity;
+  }
+`;
+
 export const ShowPost = (post: Post) => (
   <div class="relative has-menu">
-    <div class="menu absolute top-2 right-2 transition-opacity">
+    <div class="menu absolute top-2 right-2">
       <PostMenu id={post.id} isDeleted={post.isDeleted} />
     </div>
 
