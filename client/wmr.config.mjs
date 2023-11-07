b175e4bd7c42d0cb6d53ccdc22312efa6afed58d
Nanai Jua <me@n7i.dev>
2023-11-07 23:30:20 +0900

    chore(client): configure integration with vanilla-extract

--- client/wmr.config.mjs
+++ client/wmr.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'wmr';
+import { defineConfig } from "wmr";
+
+import VanillaExtract from "rollup-plugin-vanilla-extract";
 
 // Full list of options: https://wmr.dev/docs/configuration
 export default defineConfig({
-	/* Your configuration here */
-	alias: {
-		react: 'preact/compat',
-		'react-dom': 'preact/compat'
-	}
+  /* Your configuration here */
+  plugins: [VanillaExtract({ basedir: "public" })],
+
+  alias: {
+    react: "preact/compat",
+    "react-dom": "preact/compat",
+  },
 });
