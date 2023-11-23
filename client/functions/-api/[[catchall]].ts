e584e3781b1befd6d17fe3ecca09c0ec2031b25d
Nanai Jua <me@n7i.dev>
2023-11-24 06:09:27 +0900

    feat(client): add `functions/-api/[[catchall]].ts`

---
+++ client/functions/-api/[[catchall]].ts
@@ -0,0 +1,16 @@
+/// <reference types="@cloudflare/workers-types" />
+
+type Env = {
+  CF_API_BASE_URL: string;
+};
+
+export const onRequest = async (ctx: EventContext<Env, string, {}>) => {
+  const subpaths = ctx.params["catchall"];
+  if (!Array.isArray(subpaths)) {
+    return new Response(null, { status: 500 });
+  }
+
+  const url = new URL(ctx.env["CF_API_BASE_URL"] + subpaths.join("/"));
+
+  return fetch(url, { ...ctx.request });
+};
